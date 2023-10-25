69ccd6267dc829347e1098d0ef7b7331e16dbf38
刀丶剑如梦 <52901506+Gemini0303@users.noreply.github.com>
2023-10-25 10:09:17 +0800

    Update compress.go

--- internal/alg/compress/compress.go
+++ internal/alg/compress/compress.go
@@ -7,9 +7,5 @@ import (
 )
 
 func Compress(chain *[8]uint32, block *[16]uint32, counter uint64, blen uint32, flags uint32, out *[16]uint32) {
-	if consts.HasSSE41 {
-		compress_sse41.Compress(chain, block, counter, blen, flags, out)
-	} else {
-		compress_pure.Compress(chain, block, counter, blen, flags, out)
-	}
+	compress_pure.Compress(chain, block, counter, blen, flags, out)
 }
