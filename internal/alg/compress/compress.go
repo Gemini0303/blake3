16cfa02815080a4394a3e0461c796ae30479874c
zhangyuntao <zhangyuntao@coding.net>
2023-10-25 12:23:24 +0800

    fix: delete sse4

--- internal/alg/compress/compress.go
+++ internal/alg/compress/compress.go
@@ -2,8 +2,6 @@ package compress
 
 import (
 	"github.com/zeebo/blake3/internal/alg/compress/compress_pure"
-	"github.com/zeebo/blake3/internal/alg/compress/compress_sse41"
-	"github.com/zeebo/blake3/internal/consts"
 )
 
 func Compress(chain *[8]uint32, block *[16]uint32, counter uint64, blen uint32, flags uint32, out *[16]uint32) {
