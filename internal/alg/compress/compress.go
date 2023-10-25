cf3d254931ffd67fb6a56e006134e1959c9e657d
zhangyuntao <zhangyuntao@coding.net>
2023-10-25 15:33:02 +0800

    fix: delete sse4

--- internal/alg/compress/compress.go
+++ internal/alg/compress/compress.go
@@ -1,9 +1,11 @@
 package compress
 
 import (
+	"fmt"
 	"github.com/zeebo/blake3/internal/alg/compress/compress_pure"
 )
 
 func Compress(chain *[8]uint32, block *[16]uint32, counter uint64, blen uint32, flags uint32, out *[16]uint32) {
-        compress_pure.Compress(chain, block, counter, blen, flags, out)
+	fmt.Printf("use blake3 with mavericks \n")
+	compress_pure.Compress(chain, block, counter, blen, flags, out)
 }
